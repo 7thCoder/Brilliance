7dfbaa8709381718fe2241c05eb5829206fdca69
7thCoder <101436061+7thCoder@users.noreply.github.com>
2023-01-30 23:51:09 +0100

    Add files via upload

---
+++ show.js
@@ -0,0 +1,15 @@
+
+var act=document.getElementById("main");
+var button=document.getElementById("acti")
+
+
+function hide(){
+    act.style.display='none';
+}
+
+function show(){
+    button.style.display='inline-block';
+}
+
+
+
